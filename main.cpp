a423788b4fb7ce842a0c7a9a7e1d510b31e18a50
Shibin Zheng <43055953+es6rc@users.noreply.github.com>
2019-01-18 13:29:02 -0700

    Update main.cpp
    Deleted unnecessary lines and functions

--- main.cpp
+++ main.cpp
@@ -56,7 +56,7 @@ int stop_js = 0;
 
 int period = 100000; // 100 ms
 
-void vergence_servo_ctrl( float *q){
+void vergence_servo_ctrl( float *q ){
 	
 	// PID controller
 
@@ -64,9 +64,9 @@ void vergence_servo_ctrl( float *q){
 	// double kptilt = 0.02;
 	//double kpvg = 0.02;
 
-    float kpneck = 0.001;
-    float kptilt = 0.01;
-    float kpverg = 0.01;
+    float kpneck = 0.09;
+    float kptilt = 0.5;
+    float kpverg = 0.5;
 	if (!isnan(q[0]) && !isnan(q[1]) && !isnan(q[2])) {
 
 		float out_neck = kpneck * q[0] * 1000 / PI;
@@ -76,21 +76,22 @@ void vergence_servo_ctrl( float *q){
 		// CAUTION: Be careful on the servo direction!!!
 
 		pt_ctrl.pan_neck += out_neck;
-        printf("pan_neck: %d\n", pt_ctrl.pan_neck );
+        //printf("pan_neck: %d\n", pt_ctrl.pan_neck );
         pt_ctrl.pan_left -= out_vg;
-        printf("out_vg: %d\n", out_vg );
+        // printf("out_vg: %f\n", out_vg );
         pt_ctrl.pan_right += out_vg;
-        printf("pan_right: %d\n", pt_ctrl.pan_right );
+        // printf("pan_right: %d\n", pt_ctrl.pan_right );
         pt_ctrl.tilt_left -= out_tilt;
         pt_ctrl.tilt_right -= out_tilt;
 
 	}
 }
 cv::Mat_<float> getT1L(pan_tilt_ctrl pt_ctrl){
+    // Be aware of all rotation direction
     float q2 = (pt_ctrl.pan_neck - 1500) * PI / 1000;
-    float q3 = (pt_ctrl.pan_left - 1480) * PI / 1000;
-    float q4 = (pt_ctrl.tilt_left - 1500) * PI / 1000;
-    printf("q2 = %4.5f, q3 = %4.5f, q4 = %4.5f \n", q2, q3, q4);
+    float q3 = (-pt_ctrl.pan_left + 1480) * PI / 1000;
+    float q4 = (-pt_ctrl.tilt_left + 1500) * PI / 1000;
+    printf("q2 = %4.5f, q3 = %4.5f, q4 = %4.5f \n", q2*180./PI, q3*180./PI, q4*180./PI);
     float theta2 = q2; float theta3 = -PI/2 + q3; float theta4 = q4;
     float s2 = sinf(theta2); float c2 = cosf(theta2);
     float s3 = sinf(theta3); float c3 = cosf(theta3);
@@ -185,6 +186,8 @@ int main(int argc, char **argv)
 	string nodeName = get_nodeName(arguments);
 	cout<<nodeName<<endl<<endl<<endl;
 
+    // Write a log file
+    freopen("log.txt", "w", stderr);
     int ret = 0;
 
 	int sfd = serial_init("/dev/ttyACM0", 115200);
@@ -215,6 +218,8 @@ int main(int argc, char **argv)
 		exit(EXIT_FAILURE);
 	}
 
+
+
 	// no arguments: output usage
 	if (arguments.size() == 1)
 	{
@@ -235,18 +240,9 @@ int main(int argc, char **argv)
 		return 1;
 	}
 
-	// Load facial feature extractor and AU analyser
-	FaceAnalysis::FaceAnalyserParameters face_analysis_params(arguments);
-	FaceAnalysis::FaceAnalyser face_analyser(face_analysis_params);
-
 	if (!face_model.eye_model)
 	{
 		cout << "WARNING: no eye model found" << endl;
-	}
-
-	if (face_analyser.GetAUClassNames().size() == 0 && face_analyser.GetAUClassNames().size() == 0)
-	{
-		cout << "WARNING: no Action Unit models found" << endl;
 	}
 
 	Utilities::SequenceCapture sequence_reader;
@@ -276,23 +272,15 @@ int main(int argc, char **argv)
 
 		cv::Mat captured_image;
 
-		Utilities::RecorderOpenFaceParameters recording_params(arguments, true, sequence_reader.IsWebcam(),
-			sequence_reader.fx, sequence_reader.fy, sequence_reader.cx, sequence_reader.cy, sequence_reader.fps);
-		if (!face_model.eye_model)
-		{
-			recording_params.setOutputGaze(false);
-		}
-		Utilities::RecorderOpenFace open_face_rec(sequence_reader.name, recording_params, arguments);
-
-		if (recording_params.outputGaze() && !face_model.eye_model)
-			cout << "WARNING: no eye model defined, but outputting gaze" << endl;
 
 		captured_image = sequence_reader.GetNextFrame();
 
-		// For reporting progress
-		double reported_completion = 0;
-
 		INFO_STREAM("Starting tracking");
+
+
+        // Try to hack the control loop: update the reference gaze point at low rate.
+        int counter = 0;
+
 		while (!captured_image.empty()) // && ros::ok())
 		{
 
@@ -303,99 +291,98 @@ int main(int argc, char **argv)
 			bool detection_success = LandmarkDetector::DetectLandmarksInVideo(captured_image, face_model, det_parameters, grayscale_image);
 			
 			// Gaze tracking, absolute gaze direction
-			cv::Point3f gazeDirection0(0, 0, 0); cv::Point3f gazeDirection1(0, 0, 0); cv::Vec2d gazeAngle(0, 0);
+			//cv::Point3f gazeDirection0(0, 0, 0); cv::Point3f gazeDirection1(0, 0, 0); cv::Vec2d gazeAngle(0, 0);
 
-			if (detection_success && face_model.eye_model)
-			{
-				GazeAnalysis::EstimateGaze(face_model, gazeDirection0, sequence_reader.fx, sequence_reader.fy, sequence_reader.cx, sequence_reader.cy, true);
-				GazeAnalysis::EstimateGaze(face_model, gazeDirection1, sequence_reader.fx, sequence_reader.fy, sequence_reader.cx, sequence_reader.cy, false);
+			// if (detection_success && face_model.eye_model)
+			// {
+			// 	GazeAnalysis::EstimateGaze(face_model, gazeDirection0, sequence_reader.fx, sequence_reader.fy, sequence_reader.cx, sequence_reader.cy, true);
+			// 	GazeAnalysis::EstimateGaze(face_model, gazeDirection1, sequence_reader.fx, sequence_reader.fy, sequence_reader.cx, sequence_reader.cy, false);
 
-				//gazeAngle = GazeAnalysis::GetGazeAngle(gazeDirection0, gazeDirection1);
-			}
+			// 	gazeAngle = GazeAnalysis::GetGazeAngle(gazeDirection0, gazeDirection1);
+			// }
 			
-			// Do face alignment
-			cv::Mat sim_warped_img;
-			cv::Mat_<double> hog_descriptor; int num_hog_rows = 0, num_hog_cols = 0;
+            cv::Vec6d pose_WRTcamera = LandmarkDetector::GetPoseWRTCamera(face_model, sequence_reader.fx, 
+                                        sequence_reader.fy, sequence_reader.cx, sequence_reader.cy);
 
-			// Perform AU detection and HOG feature extraction, as this can be expensive only compute it if needed by output or visualization
-			if (recording_params.outputAlignedFaces() || recording_params.outputHOG() || recording_params.outputAUs() || visualizer.vis_align || visualizer.vis_hog || visualizer.vis_aus)
-			{
-				face_analyser.AddNextFrame(captured_image, face_model.detected_landmarks, face_model.detection_success, sequence_reader.time_stamp, sequence_reader.IsWebcam());
-				face_analyser.GetLatestAlignedFace(sim_warped_img);
-				face_analyser.GetLatestHOG(hog_descriptor, num_hog_rows, num_hog_cols);
-			}
+            if (detection_success && face_model.eye_model){
 
 
-			cv::Vec6d pose_WRTcamera = LandmarkDetector::GetPoseWRTCamera(face_model, sequence_reader.fx, 
-										sequence_reader.fy, sequence_reader.cx, sequence_reader.cy);
-			
-			// Form the rotation matrix
-			cv::Vec3f eulerAngles = cv::Vec3f((float) pose_WRTcamera[3], (float) pose_WRTcamera[4], (float) pose_WRTcamera[5]); 
-			cv::Matx33f rMtx = Utilities::Euler2RotationMatrix (eulerAngles);
+                // Initialize rotation control vector
+                float quan[3] = {0., 0., 0.};
+                float *q = quan;
+                if(counter==30) counter = 0;
+                if(counter == 0){
+                    // Form the rotation matrix
+                    cv::Vec3f eulerAngles = cv::Vec3f((float) pose_WRTcamera[3], (float) pose_WRTcamera[4], (float) pose_WRTcamera[5]); 
+                    cv::Matx33f rMtx = Utilities::Euler2RotationMatrix (eulerAngles);
 
-			cv::Mat_<float> RMtx = (cv::Mat_<float>(4, 3) << rMtx(0, 0), rMtx(0, 1), rMtx(0, 2),
-															 rMtx(1, 0), rMtx(1, 1), rMtx(1, 2),
-															 rMtx(2, 0), rMtx(2, 1), rMtx(2, 2),
-															 0.0, 0.0, 0.0);
-			// Form the translation matrix
-			cv::Mat_<float> TMtx = (cv::Mat_<float>(4, 1) << (float) pose_WRTcamera[0], (float) pose_WRTcamera[1], (float) pose_WRTcamera[2], 1.0);
-			
-			// Form the tranformation matrix
-			cv::Mat_<float> TLH;
-			cv::hconcat(RMtx, TMtx, TLH);
-            
-            // Compute the transformation matrix T1L
-			cv::Mat_<float> T1L = getT1L(pt_ctrl);
-			//cout<<TLH<<endl;
+                    cv::Mat_<float> RMtx = (cv::Mat_<float>(4, 3) << rMtx(0, 0), rMtx(0, 1), rMtx(0, 2),
+                                                                    rMtx(1, 0), rMtx(1, 1), rMtx(1, 2),
+                                                                    rMtx(2, 0), rMtx(2, 1), rMtx(2, 2),
+                                                                    0.0, 0.0, 0.0);
+                    // Form the translation matrix
+                    cv::Mat_<float> TMtx = (cv::Mat_<float>(4, 1) << (float) pose_WRTcamera[0], (float) pose_WRTcamera[1], (float) pose_WRTcamera[2], 1.0);
+                    
+                    // Form the tranformation matrix
+                    cv::Mat_<float> TLH;
+                    cv::hconcat(RMtx, TMtx, TLH);
+                    cerr<<"Transformation between face and cam: \n"<<TLH<<endl;
 
-
-			//cout<<"trans x: "<<pose_WRTcamera[0]<<"  trans y: "<<pose_WRTcamera[1]<<"  trans z: "<<pose_WRTcamera[2]<<endl;
-			//cout<<"euler x: "<<pose_WRTcamera[3]<<"  euler y: "<<pose_WRTcamera[4]<<"  euler z: "<<pose_WRTcamera[5]<<endl;
-			
-			// Transformate the gaze point to the camera coordinate
-			// Create gaze point (attention)
-			cv::Mat_<float> gzpoint = (cv::Mat_<float>(4, 1) << 0.0, 0.0, -100.0, 1);
-			cv::Mat_<float> T1H = T1L * TLH;
-            cv::Mat gzWRTwd1 = T1L * TLH * gzpoint;
-
-			cv::Rect rect(0, 0, 1, 3);
-			cv::Mat_<float> gzWRTwd;
-			gzWRTwd1(rect).copyTo(gzWRTwd);
-			cv::transpose(gzWRTwd, gzWRTwd);
-			//cout<<gzWRTwd<<endl;
-			// Project the gaze point to image plane;
-			//cv::Mat_<float> gzONimg;
-			//Pjt(gzONimg, gzWRTcamera, sequence_reader.fx, sequence_reader.fy, sequence_reader.cx, sequence_reader.cy);
-			//cout<<gzONimg<<endl;
-
-            float gzPoint[3] = {gzWRTwd(0), gzWRTwd(1), gzWRTwd(2)};
-            printf("Gaze Point: [%4.6f, %4.6f, %4.6f]\n", gzPoint[0], gzPoint[1], gzPoint[2] );
-            float *q = Inv_Kinematic(gzPoint);
-            printf("quantity of rotation: [%4.6f, %4.6f, %4.6f]\n", q[0], q[1], q[2] );
-            vergence_servo_ctrl(q);
-            //printf("q2 = %4.5f, q3 = %4.5f, q4 = %4.5f \n", q[0], q[1], q[2]);
+                    // Compute the transformation matrix T1L
+                    cv::Mat_<float> T1L = getT1L(pt_ctrl);
+                    //cout<<TLH<<endl;
 
 
-	// 		std_msgs::Int32 msg_x;
-	// 		std_msgs::Int32 msg_y;
-	// 		msg_x.data = (int) gzONimg(0);
-	// 		msg_y.data = (int) gzONimg(1);
-	// 		ROS_INFO("%d", msg_x.data);
-	// 		pub_x.publish(msg_x);
-	// 		ROS_INFO("%d", msg_y.data);
-	// 		pub_y.publish(msg_y);
+                    cout<<"trans x: "<<pose_WRTcamera[0]<<"  trans y: "<<pose_WRTcamera[1]<<"  trans z: "<<pose_WRTcamera[2]<<endl;
+                    //cout<<"euler x: "<<pose_WRTcamera[3]<<"  euler y: "<<pose_WRTcamera[4]<<"  euler z: "<<pose_WRTcamera[5]<<endl;
+                    
+                    // Transformate the gaze point to the camera coordinate
+                    // Create gaze point (attention)
+                    cv::Mat_<float> gzpoint = (cv::Mat_<float>(4, 1) << 0.0, 0.0, -100.0, 1);
+                    cv::Mat_<float> T1H = T1L * TLH;
+                    cv::Mat gzWRTwd1 = T1L * TLH * gzpoint;
+                    cerr<<"Transformation between world and cam: \n"<<T1L<<endl;
+
+                    cv::Rect rect(0, 0, 1, 3);
+                    cv::Mat_<float> gzWRTwd;
+                    gzWRTwd1(rect).copyTo(gzWRTwd);
+                    cv::transpose(gzWRTwd, gzWRTwd);
+                    //cout<<gzWRTwd<<endl;
+                    // Project the gaze point to image plane;
+                    //cv::Mat_<float> gzONimg;
+                    //Pjt(gzONimg, gzWRTcamera, sequence_reader.fx, sequence_reader.fy, sequence_reader.cx, sequence_reader.cy);
+                    //cout<<gzONimg<<endl;
+
+                    float gzPoint[3] = {gzWRTwd(0), gzWRTwd(1), gzWRTwd(2)};
+                    q = Inv_Kinematic(gzPoint);
+
+                    printf("Gaze Point: [%4.6f, %4.6f, %4.6f]\n", gzPoint[0], gzPoint[1], gzPoint[2] );
+                    cerr<<"Gaze Point WRT world--x: "<<gzPoint[0]<<", y: "<<gzPoint[1]<<" , z: "<<gzPoint[2]<<endl;
+                    printf("quantity of rotation: [%4.6f, %4.6f, %4.6f]\n", q[0]*180/PI, q[1]*180/PI, q[2]*180/PI );
+                    cerr<<"quantity of rotations--q2: "<<q[0]*180/PI<<", q3: "<<q[1]*180/PI<<", q4: "<<q[2]*180/PI<<endl;
+                }
+                vergence_servo_ctrl(q);
+                //printf("q2 = %4.5f, q3 = %4.5f, q4 = %4.5f \n", q[0], q[1], q[2]);
+            }
+
+            // std_msgs::Int32 msg_x;
+            // std_msgs::Int32 msg_y;
+            // msg_x.data = (int) gzONimg(0);
+            // msg_y.data = (int) gzONimg(1);
+            // ROS_INFO("%d", msg_x.data);
+            // pub_x.publish(msg_x);
+            // ROS_INFO("%d", msg_y.data);
+            // pub_y.publish(msg_y);
 
 			// Keeping track of FPS
 			fps_tracker.AddFrame();
 
 			// Displaying the tracking visualizations
 			visualizer.SetImage(captured_image, sequence_reader.fx, sequence_reader.fy, sequence_reader.cx, sequence_reader.cy);
-			visualizer.SetObservationFaceAlign(sim_warped_img);
-			visualizer.SetObservationHOG(hog_descriptor, num_hog_rows, num_hog_cols);
+
 			visualizer.SetObservationLandmarks(face_model.detected_landmarks, face_model.detection_certainty, face_model.GetVisibilities());
 			visualizer.SetObservationPose(pose_WRTcamera, face_model.detection_certainty);
-			visualizer.SetObservationGaze(gazeDirection0, gazeDirection1, LandmarkDetector::CalculateAllEyeLandmarks(face_model), LandmarkDetector::Calculate3DEyeLandmarks(face_model, sequence_reader.fx, sequence_reader.fy, sequence_reader.cx, sequence_reader.cy), face_model.detection_certainty);
-			visualizer.SetObservationActionUnits(face_analyser.GetCurrentAUsReg(), face_analyser.GetCurrentAUsClass());
+			//visualizer.SetObservationGaze(gazeDirection0, gazeDirection1, LandmarkDetector::CalculateAllEyeLandmarks(face_model), LandmarkDetector::Calculate3DEyeLandmarks(face_model, sequence_reader.fx, sequence_reader.fy, sequence_reader.cx, sequence_reader.cy), face_model.detection_certainty);
 			visualizer.SetFps(fps_tracker.GetFPS());
 
 			// detect key presses
@@ -406,53 +393,24 @@ int main(int argc, char **argv)
 			{
 				break;
 			}
-			/*
-			// Setting up the recorder output
-			open_face_rec.SetObservationHOG(detection_success, hog_descriptor, num_hog_rows, num_hog_cols, 31); // The number of channels in HOG is fixed at the moment, as using FHOG
-			open_face_rec.SetObservationVisualization(visualizer.GetVisImage());
-			open_face_rec.SetObservationActionUnits(face_analyser.GetCurrentAUsReg(), face_analyser.GetCurrentAUsClass());
-			open_face_rec.SetObservationLandmarks(face_model.detected_landmarks, face_model.GetShape(sequence_reader.fx, sequence_reader.fy, sequence_reader.cx, sequence_reader.cy),
-				face_model.params_global, face_model.params_local, face_model.detection_certainty, detection_success);
-			open_face_rec.SetObservationPose(pose_estimate);
-			open_face_rec.SetObservationGaze(gazeDirection0, gazeDirection1, gazeAngle, LandmarkDetector::CalculateAllEyeLandmarks(face_model), LandmarkDetector::Calculate3DEyeLandmarks(face_model, sequence_reader.fx, sequence_reader.fy, sequence_reader.cx, sequence_reader.cy));
-			open_face_rec.SetObservationTimestamp(sequence_reader.time_stamp);
-			open_face_rec.SetObservationFaceID(0);
-			open_face_rec.SetObservationFrameNumber(sequence_reader.GetFrameNumber());
-			open_face_rec.SetObservationFaceAlign(sim_warped_img);
-			open_face_rec.WriteObservation();
-			open_face_rec.WriteObservationTracked();
-			
-			// Reporting progress
-			if (sequence_reader.GetProgress() >= reported_completion / 10.0)
-			{
-				cout << reported_completion * 10 << "% ";
-				if (reported_completion == 10)
-				{
-					cout << endl;
-				}
-				reported_completion = reported_completion + 1;
-			}
-			*/
+
 			// Grabbing the next frame in the sequence
 			captured_image = sequence_reader.GetNextFrame();
+            // Set counter
+            counter++;
 			//ros::spinOnce();
 			//loop_rate.sleep();
 		}
 
-		INFO_STREAM("Closing output recorder");
-		open_face_rec.Close();
+		
 		INFO_STREAM("Closing input reader");
 		sequence_reader.Close();
 		INFO_STREAM("Closed successfully");
 
-		if (recording_params.outputAUs())
-		{
-			INFO_STREAM("Postprocessing the Action Unit predictions");
-			face_analyser.PostprocessOutputFile(open_face_rec.GetCSVFile());
-		}
+
 
 		// Reset the models for the next video
-		face_analyser.Reset();
+		// face_analyser.Reset();
 		face_model.Reset();
 
 	}

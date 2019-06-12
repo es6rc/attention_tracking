f5f2428b235340a17d983d94906ceb576ba76c70
Shibin Zheng <43055953+es6rc@users.noreply.github.com>
2019-06-12 00:54:14 -0700

    Update cmake_command.sh

--- cmake_command.sh
+++ cmake_command.sh
@@ -1,1 +1,2 @@
-cmake -DOpenFace_DIR=/home/z/CppDependencies/OpenFace/install/lib/cmake/OpenFace/  CMAKE_BUILD_TYPE=RELEASE  -DCMAKE_CXX_FLAGS="-std=c++11" -DCMAKE_EXE_LINKER_FLAGS="-std=c++11"  ../
+cmake -DOpenFace_DIR=/home/z/CppDependencies/OpenFace/install/lib/cmake/OpenFace/ \
+CMAKE_BUILD_TYPE=RELEASE  -DCMAKE_CXX_FLAGS="-std=c++11" -DCMAKE_EXE_LINKER_FLAGS="-std=c++11"  ../
